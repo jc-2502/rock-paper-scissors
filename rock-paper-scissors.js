6cd4cc6dc817697372cd8cec16f4eff9c6023701
jc-2502 <16088146+jc-2502@users.noreply.github.com>
2025-08-08 18:06:53 -0400

    Write new version of getComputerChoice
    Use switch statement for one expression - slightly cleaner

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,10 +1,21 @@
+// function getComputerChoice() {
+//   let randomNumber = Math.floor(Math.random() * 3) + 1;
+//   if (randomNumber === 1) {
+//     return 'rock';
+//   } else if (randomNumber === 2) {
+//     return 'paper';
+//   } else {
+//     return 'scissors';
+//   }
+// }
+
 function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3) + 1;
-  if (randomNumber === 1) {
-    return 'rock';
-  } else if (randomNumber === 2) {
-    return 'paper';
-  } else {
-    return 'scissors';
+  switch (Math.floor(Math.random() * 3) + 1) {
+    case 1:
+      return 'rock';
+    case 2:
+      return 'paper';
+    case 3:
+      return 'scissors';
   }
 }
