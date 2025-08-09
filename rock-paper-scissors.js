bcb70145f220d10c4761180b6cc6a781ccd2a8d8
jc-2502 <16088146+jc-2502@users.noreply.github.com>
2025-08-08 20:24:12 -0400

    Add playRound function
    Add function to play one game:
    Determine winner
    Print message
    Increment score
    
    Also update message in getHumanChoice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -21,7 +21,7 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  let input = parseInt(prompt('enter 1/2/3:'));
+  let input = parseInt(prompt('enter 1/2/3 for rock/paper/scisssors:'));
   switch (input) {
     case 1:
       return 'rock';
@@ -35,3 +35,49 @@ function getHumanChoice() {
 // keep track of score
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  let winner;
+  if (humanChoice === 'rock') {
+    if (computerChoice === 'rock') {
+      winner = 'tie';
+    } else if (computerChoice === 'paper') {
+      winner = 'computer';
+    } else if (computerChoice === 'scissors') {
+      winner = 'human';
+    }
+  } else if (humanChoice === 'paper') {
+    if (computerChoice === 'rock') {
+      winner = 'human';
+    } else if (computerChoice === 'paper') {
+      winner = 'tie';
+    } else if (computerChoice === 'scissors') {
+      winner = 'computer';
+    }
+  } else if (humanChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      winner = 'computer';
+    } else if (computerChoice === 'paper') {
+      winner = 'human';
+    } else if (computerChoice === 'scissors') {
+      winner = 'tie';
+    }
+  }
+
+  let message;
+  if (winner === 'human') {
+    message = 'you won - '+humanChoice+' beats '+computerChoice;
+  } else if (winner === 'computer') {
+    message = 'you lost - '+computerChoice+' beats '+humanChoice;
+  } else {
+    message = 'tie - '+humanChoice;
+  }
+  console.log(message);
+
+  ++humanScore;
+  ++computerScore;
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
